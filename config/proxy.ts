22930869c5840c35e1616a0204ddd8ad67e4860a
lxy <499218100@qq.com>
2021-12-01 23:02:18 +0800

    init

---
+++ config/proxy.ts
@@ -0,0 +1,32 @@
+/**
+ * 在生产环境 代理是无法生效的，所以这里没有生产环境的配置
+ * -------------------------------
+ * The agent cannot take effect in the production environment
+ * so there is no configuration of the production environment
+ * For details, please see
+ * https://pro.ant.design/docs/deploy
+ */
+export default {
+  dev: {
+    "/api/": {
+      // 要代理的地址
+      target: "http://127.0.0.1:9090/",
+      changeOrigin: true,
+      pathRewrite: { "^": "" },
+    },
+  },
+  test: {
+    "/api/": {
+      target: "https://proapi.azurewebsites.net",
+      changeOrigin: true,
+      pathRewrite: { "^": "" },
+    },
+  },
+  pre: {
+    "/api/": {
+      target: "your pre url",
+      changeOrigin: true,
+      pathRewrite: { "^": "" },
+    },
+  },
+};
